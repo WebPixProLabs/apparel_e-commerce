fddbfec1d7aaf8f635e7b8a91515793cd977771d
ak220193 <mail2meak22frcrio@gmail.com>
2024-09-29 20:55:48 +0530

    Our Policy Component

---
+++ frontend/src/Components/OurPolicy.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { assets } from "../assets/frontend_assets/assets"
+
+const OurPolicy = () => {
+  return (
+    <div className='flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base tex-gray-700'>
+
+    <div>
+    <img src={assets.exchange_icon} alt="" className='w-12 m-auto mb-5' />
+    <p className='font-semibold'> Easy Exchange Policy </p>
+    <p className='text-gray-400'>We Offer Hassle Free Exchange Policy !</p>
+    </div>
+
+    <div>
+    <img src={assets.quality_icon} alt="" className='w-12 m-auto mb-5' />
+    <p className='font-semibold'> 7 Days Return Policy </p>
+    <p className='text-gray-400'>We Provide 7 Days Free Return Policy </p>
+    </div>
+
+    <div>
+    <img src={assets.support_img} alt="" className='w-12 m-auto mb-5' />
+    <p className='font-semibold'>Best Customer Support </p>
+    <p className='text-gray-400'>We Provide 24 X 7 Customer Support </p>
+    </div>
+
+    </div>
+  )
+}
+
+export default OurPolicy

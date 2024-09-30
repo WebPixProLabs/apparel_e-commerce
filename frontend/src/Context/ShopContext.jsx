21bdc9ec3e7227d39f44ac5a232d641772ddeb59
ak220193 <mail2meak22frcrio@gmail.com>
2024-09-30 19:56:33 +0530

    Collection Page Completed With Filters

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import { products } from "../assets/frontend_assets/assets.js";
 
 export const ShopContext = createContext();
@@ -6,11 +6,17 @@ export const ShopContext = createContext();
 export const ShopContextProvider = ({ children }) => {
   const currency = "₹";
   const delivery_fee = 50;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
 
   const value = {
     products,
     currency,
     delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
   };
 
   return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;

35faab34e436ab0de9c7abdaad5607a2f0ff2e51
ak220193 <mail2meak22frcrio@gmail.com>
2024-10-01 09:49:25 +0530

    collection Page Along with Decription

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -6,8 +6,8 @@ import { ShopContext } from "../Context/ShopContext.jsx";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
-  const { setShowSearch } = useContext(ShopContext);
- 
+  const { setShowSearch, getCartCount } = useContext(ShopContext);
+
   return (
     <div className="flex items-center justify-between  py-5 font-medium">
       <Link to="/">
@@ -34,8 +34,13 @@ const Navbar = () => {
       </ul>
 
       <div className="flex items-center gap-6">
-        <img src={assets.search_icon} alt="" className="w-5 cursor-pointer" onClick={()=>setShowSearch(true)} />
- 
+        <img
+          src={assets.search_icon}
+          alt=""
+          className="w-5 cursor-pointer"
+          onClick={() => setShowSearch(true)}
+        />
+
         <div className="group relative ">
           <img
             src={assets.profile_icon}
@@ -54,7 +59,7 @@ const Navbar = () => {
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
-            10
+            {getCartCount()}
           </p>
         </Link>
 
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets.js";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -8,6 +9,42 @@ export const ShopContextProvider = ({ children }) => {
   const delivery_fee = 50;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = async (itemId, size) => {
+    if (!size) {
+      toast.error("Select Product Size");
+      return;
+    }
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+    setCartItems(cartData);
+  };
+
+  const getCartCount = () => {
+    let totalcount = 0;
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalcount += cartItems[items][item];
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    }
+    return totalcount;
+  };
 
   const value = {
     products,
@@ -17,6 +54,9 @@ export const ShopContextProvider = ({ children }) => {
     setSearch,
     showSearch,
     setShowSearch,
+    cartItems,
+    addToCart,
+    getCartCount,
   };
 
   return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;

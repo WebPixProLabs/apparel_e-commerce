c3d1691c8e29a3a22f5c4475babca5e4f593d1db
ak220193 <mail2meak22frcrio@gmail.com>
2024-10-08 17:24:14 +0530

    Admin Login api Completed

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,18 +1,19 @@
 import express from "express";
 import { addProduct, listProducts, removeProduct, singleProduct } from "../controllers/productController.js";
 import upload from "../middlewares/multer.js";
+import adminAuth from "../middlewares/adminAuth.js";
 
 
 
 const productRouter = express.Router();
 
-productRouter.post('/add', upload.fields([
+productRouter.post('/add',adminAuth, upload.fields([
     { name: 'image1', maxCount: 1 },
     { name: 'image2', maxCount: 1 },
     { name: 'image3', maxCount: 1 },
     { name: 'image4', maxCount: 1 }
   ]), addProduct);
-productRouter.post('/remove',removeProduct);
+productRouter.post('/remove',adminAuth,removeProduct);
 productRouter.post('/single',singleProduct);
 productRouter.get('/list',listProducts);
 

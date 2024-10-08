3bc9a405d8f09a89804308a0aea38cb06d61c22e
ak220193 <mail2meak22frcrio@gmail.com>
2024-10-08 19:24:00 +0530

    Routing Setup Complete

---
+++ admin/src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { NavLink } from "react-router-dom"
+import { assets } from "../assets/admin_assets/assets.js"
+
+const Sidebar = () => {
+  return (
+   <div className='w-[18%] min-h-screen border-r-2'>
+    <div className='flex flex-col gap-6 pt-6 pl-[20%] text-[15px]'>
+        <NavLink to="/add" className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l">
+            <img src={assets.add_icon} className='w-5 h-5' alt="" />
+            <p className='hidden md:block'>Add Items </p>
+        </NavLink>
+        <NavLink to="/list" className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l">
+            <img src={assets.order_icon} className='w-5 h-5' alt="" />
+            <p className='hidden md:block'>List Items </p>
+        </NavLink>
+        <NavLink to="/orders" className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l">
+            <img src={assets.order_icon} className='w-5 h-5' alt="" />
+            <p className='hidden md:block'> Orders </p>
+        </NavLink>
+    </div>
+   </div>
+  )
+}
+
+export default Sidebar

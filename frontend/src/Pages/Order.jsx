8f9488f50105d018f882cd6f6aab1cdd2bac2d7b
ak220193 <mail2meak22frcrio@gmail.com>
2024-10-17 20:27:34 +0530

    login , logout,orders complete

--- frontend/src/Pages/Order.jsx
+++ frontend/src/Pages/Order.jsx
@@ -18,7 +18,7 @@ const Order = () => {
             className="py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4"
           >
             <div className="flex items-start gap-6 text-sm">
-              <img src={item.image[0]} alt="" className="w-16 sm:w-20" />
+              <img src={item.images[0]} alt="" className="w-16 sm:w-20" />
               <div>
                 <p className="sm:text-base font-medium">{item.name}</p>
                 <div className="flex items-center gap-3 mt-2 text-base ext-gray-700">

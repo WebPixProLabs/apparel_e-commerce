2698d9be1ab0c4747cc59884faaca9b73b63bb36
ak220193 <mail2meak22frcrio@gmail.com>
2024-11-01 18:53:22 +0530

    login error fix

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import authUser from '../middlewares/auth.js';
+
 import { addToCart, updateCart, getUserCart } from "../controllers/cartController.js";
 
 const cartRouter = express.Router();
 
 
-cartRouter.post("/get", authUser, getUserCart);  
-cartRouter.post("/add",authUser, addToCart);  
-cartRouter.post("/update",authUser, updateCart); 
+cartRouter.post("/get", getUserCart);  
+cartRouter.post("/add", addToCart);  
+cartRouter.post("/update", updateCart); 
 
 export default cartRouter;

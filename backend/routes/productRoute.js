ffc96ee0bc524cfcca2414cbcd56ac0bbecf7b10
ak220193 <mail2meak22frcrio@gmail.com>
2024-11-17 12:07:37 +0530

    product Route Changes

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -7,13 +7,13 @@ import adminAuth from "../middlewares/adminAuth.js";
 
 const productRouter = express.Router();
 
-productRouter.post('/add',adminAuth,upload.fields([
+productRouter.post('/add',upload.fields([
     { name: 'image1', maxCount: 1 },
     { name: 'image2', maxCount: 1 },
     { name: 'image3', maxCount: 1 },
     { name: 'image4', maxCount: 1 }
   ]), addProduct);
-productRouter.delete('/remove/:id',adminAuth,removeProduct);
+productRouter.delete('/remove/:id',removeProduct);
 productRouter.post('/single',singleProduct);
 productRouter.get('/list',listProducts);
 

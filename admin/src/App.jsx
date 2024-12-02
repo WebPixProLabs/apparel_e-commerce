109f7336f03428003cbc1872baa82c8c5abc594a
ak220193 <mail2meak22frcrio@gmail.com>
2024-12-02 19:07:57 +0530

    orders api in admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,9 @@ import Login from "./components/Login";
 import toast, { Toaster } from 'react-hot-toast';
 
 
-export const backendUrl = import.meta.env.VITE_BACKEND_URL
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const currency = '₹';
+
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem("token") : '');
 
